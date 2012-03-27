fbffa6cf8b622e38009103aa631029e1e49d247b
Kartikeya <kartikeyadubey@gmail.com>
2012-03-26 20:58:25 -0400

    Both machines connected

--- Share/MainWindow.xaml.cs
+++ Share/MainWindow.xaml.cs
@@ -26,22 +26,59 @@ namespace Share
     /// </summary>
     public partial class MainWindow : Window
     {
-        BackgroundWorker _worker = new BackgroundWorker();
-        BackgroundWorker _serverThread = new BackgroundWorker();
-        SensorData _sensor;
-        TextBlock greeting;
-        bool isGreeting = false;
+        #region Threads
+        BackgroundWorker _worker;
+        BackgroundWorker _serverThread;
+        BackgroundWorker _clientThread;
+        #endregion
+
+        #region Constants
         private const int IMAGE_WIDTH = 640;
         private const int IMAGE_HEIGHT = 480;
         private const int IMAGE_SIZE = 640*480*4;
-        int port = 3000;
-        bool test = false;
-        bool imageSent = false;
+        private const int portSend = 3000;
+        private const int portReceive = 4000;
+        private const int DPI_X = 96;
+        private const int DPI_Y = 96;
+        private const int TRANSMIT_IMAGE_SIZE = 320 * 240 * 4;
+        private const int TRANSMIT_WIDTH = 320;
+        private const int TRANSMIT_HEIGHT = 240;
+        #endregion
+
+        #region Server Variables
         Socket s;
         bool clientConnected = false;
         bool backgroundSent = false;
         WriteableBitmap clientImage;
         int imageSendCounter;
+        bool imageSent = false;
+        #endregion
+
+        #region Client Variables
+        TcpClient client;
+        IPEndPoint serverEndPoint;
+        NetworkStream clientStream;
+        private bool connectedToServer = false;
+        private const string ipAddress = "127.0.0.1";
+        private bool backgroundReceived = false;
+        byte[] backgroundImage;
+        byte[] partialPlayerImage;
+        byte[] playerImage;
+
+        private int xStart;
+        private int xEnd;
+        private int yStart;
+        private int yEnd;
+        WriteableBitmap serverImage;
+        private byte[] COMPLETE_MESSAGE;
+        private byte[] HELLO_MESSAGE;
+        #endregion
+
+        #region Display variables
+        SensorData _sensor;
+        TextBlock greeting;
+        bool isGreeting = false;
+        #endregion
 
         public MainWindow()
         {
@@ -56,10 +93,187 @@ namespace Share
             CompositionTarget.Rendering += new EventHandler(CompositionTarget_Rendering);
             Closing += new System.ComponentModel.CancelEventHandler(MainWindow_Closing);
 
+            _worker = new BackgroundWorker();
             _worker.DoWork += new DoWorkEventHandler(Worker_DoWork);
+
+            _serverThread = new BackgroundWorker();
             _serverThread.DoWork +=new DoWorkEventHandler(_serverThread_DoWork);
-            //clientImage = new WriteableBitmap(IMAGE_WIDTH, IMAGE_HEIGHT, 96, 96, PixelFormats.Bgra32, null);
+
+            _clientThread = new BackgroundWorker();
+            _clientThread.DoWork += new DoWorkEventHandler(_clientThread_DoWork);
+
             imageSendCounter = 1;
+            serverImage = new WriteableBitmap(TRANSMIT_WIDTH, TRANSMIT_HEIGHT, DPI_X, DPI_Y, PixelFormats.Bgra32, null);
+            ASCIIEncoding enc = new ASCIIEncoding();
+            COMPLETE_MESSAGE = enc.GetBytes("rcomplete");
+            HELLO_MESSAGE = enc.GetBytes("hello");
+        }
+
+        void _clientThread_DoWork(object sender, DoWorkEventArgs e)
+        {
+            ASCIIEncoding encoder = new ASCIIEncoding();
+            if (!connectedToServer && clientConnected)
+            {
+                client = new TcpClient();
+                serverEndPoint = new IPEndPoint(IPAddress.Parse(ipAddress), portReceive);
+
+                client.Connect(serverEndPoint);
+                clientStream = client.GetStream();
+
+                clientStream.Write(HELLO_MESSAGE, 0, HELLO_MESSAGE.Length);
+                clientStream.Flush();
+                Console.WriteLine("Client said hello");
+
+                clientStream.Write(COMPLETE_MESSAGE, 0, COMPLETE_MESSAGE.Length);
+                clientStream.Flush();
+                connectedToServer = true;
+            }
+            else if(connectedToServer)
+            {
+                byte[] backgroundImageData = new byte[TRANSMIT_IMAGE_SIZE];
+                byte[] serverMessage = new byte[65535];
+                byte[] xS = new byte[4];
+                byte[] xE = new byte[4];
+                byte[] yS = new byte[4];
+                byte[] yE = new byte[4];
+                int bytesRead;
+
+                while (true)
+                {
+                    bytesRead = 0;
+
+                    try
+                    {
+                        if (!client.Connected)
+                        {
+                            client.Connect(serverEndPoint);
+                        }
+                        clientStream.Flush();
+                        //blocks until a server replies with a message
+                        if (!backgroundReceived)
+                        {
+                            bytesRead = clientStream.Read(backgroundImageData, 0, TRANSMIT_IMAGE_SIZE);
+                        }
+                        else
+                        {
+                            bytesRead = clientStream.Read(serverMessage, 0, serverMessage.Length);
+                        }
+                    }
+                    catch
+                    {
+                        //a socket error has occured
+                        Console.WriteLine("Socket error has occured");
+                    }
+
+                    if (bytesRead == 0)
+                    {
+                        //the client has disconnected from the server
+                        Console.WriteLine("Player has disconnected from the server");
+                    }
+
+                    //If the background has not been received yet
+                    //Copy it and store it for future use
+                    if (!backgroundReceived)
+                    {
+                        backgroundImage = new byte[backgroundImageData.Length];
+                        Array.Copy(backgroundImageData, backgroundImage, backgroundImageData.Length);
+                        backgroundReceived = true;
+                        Console.WriteLine("Background received");
+                        clientStream.Flush();
+                        drawPlayer();
+                    }
+                    else if (encoder.GetString(serverMessage, 0, bytesRead) == "playerimage")
+                    {
+                        clientStream.Flush();
+                        bytesRead = clientStream.Read(xS, 0, 4);
+                        xStart = BitConverter.ToInt32(xS, 0);
+
+                        clientStream.Flush();
+                        bytesRead = clientStream.Read(xE, 0, 4);
+                        xEnd = BitConverter.ToInt32(xE, 0);
+
+                        clientStream.Flush();
+                        bytesRead = clientStream.Read(yS, 0, 4);
+                        yStart = BitConverter.ToInt32(yS, 0);
+
+                        clientStream.Flush();
+                        bytesRead = clientStream.Read(yE, 0, 4);
+                        yEnd = BitConverter.ToInt32(yE, 0);
+
+                        clientStream.Flush();
+                        byte[] byteImgSize = new byte[4];
+                        bytesRead = clientStream.Read(byteImgSize, 0, 4);
+                        int imgSize = BitConverter.ToInt32(byteImgSize, 0);
+
+                        clientStream.Flush();
+                        partialPlayerImage = new byte[imgSize];
+                        playerImage = new byte[imgSize];
+
+                        bytesRead = 0;
+                        while (bytesRead != imgSize)
+                        {
+                            int tmpBytesRead = clientStream.Read(partialPlayerImage, 0, imgSize);
+                            Buffer.BlockCopy(partialPlayerImage, 0, playerImage, bytesRead, tmpBytesRead);
+                            bytesRead += tmpBytesRead;
+                        }
+
+                        drawPlayer();
+                        
+                        clientStream.Write(COMPLETE_MESSAGE, 0, COMPLETE_MESSAGE.Length);
+                        clientStream.Flush();
+                        //Console.WriteLine("Image received, size of image: " + imgSize + "," + xStart + ", " + xEnd + ", " + yStart + ", " + yEnd);
+                    }
+                }
+            }
+        }
+
+        void drawPlayer()
+        {
+            unsafe
+            {
+                Dispatcher.Invoke((Action)delegate
+                {
+                    serverImage.Lock();
+                    int dataCounter = 0;
+                    //Draw background image
+                    for (int y = 0; y < TRANSMIT_HEIGHT; ++y)
+                    {
+                        byte* pDest = (byte*)serverImage.BackBuffer.ToPointer() + y * serverImage.BackBufferStride;
+                        for (int x = 0; x < TRANSMIT_WIDTH; ++x, pDest += 4, dataCounter += 4)
+                        {
+                            pDest[0] = backgroundImage[dataCounter];
+                            pDest[1] = backgroundImage[dataCounter + 1];
+                            pDest[2] = backgroundImage[dataCounter + 2];
+                            pDest[3] = backgroundImage[dataCounter + 3];
+                        }
+                    }
+
+                    dataCounter = 0;
+                    //WORKING CODE UNCOMMENT AFTER TESTING COMPRESSION
+                    if (playerImage != null)
+                    {
+                        //Draw new updated image
+                        for (int y = yStart; y < yEnd; ++y)
+                        {
+                            byte* pDest = (byte*)serverImage.BackBuffer.ToPointer() + (y * serverImage.BackBufferStride);
+                            pDest += 4 * xStart;
+                            for (int x = xStart; x < xEnd; ++x, pDest += 4, dataCounter += 4)
+                            {
+                                pDest[0] = playerImage[dataCounter];
+                                pDest[1] = playerImage[dataCounter + 1];
+                                pDest[2] = playerImage[dataCounter + 2];
+                                pDest[3] = playerImage[dataCounter + 3];
+                            }
+                        }
+                    }
+
+                    serverImage.AddDirtyRect(new Int32Rect(0, 0, TRANSMIT_WIDTH, TRANSMIT_HEIGHT));
+                    //Console.WriteLine("Draw Image now");
+                    image2.Source = serverImage;
+                    //imageDrawn = true;
+                    serverImage.Unlock();
+                });
+            }
         }
 
         void _sensor_imageUpdate(object sender, WriteableBitmap b, bool playerFound, int xStart, int xEnd, int yStart, int yEnd)
@@ -80,17 +294,25 @@ namespace Share
                     ASCIIEncoding encoder = new ASCIIEncoding();
                     byte[] buffer = encoder.GetBytes("playerimage");
 
+                    byte[] readyToReceive = encoder.GetBytes("rcomplete");
+                    clientStream.Write(readyToReceive, 0, readyToReceive.Length);
+                    clientStream.Flush();
                     byte[] completeMessage = new byte[65536];
+                    
                     int k = s.Receive(completeMessage);
                     while (k == 0)
                     {
                         k = s.Receive(completeMessage);
                     }
-                    Console.WriteLine("Message received: " + encoder.GetString(completeMessage, 0, k));
-
-
-                    //int imageSize = ((xEnd - xStart) * (yEnd - yStart) * 4);
-                    //byte[] playerImage = new byte[(imageSize)];
+                    //Get the string from the first 9 bytes since
+                    //rcomplete may have been appended to ensure there is no deadlock
+                    string tempMessage = encoder.GetString(completeMessage, 0, 9);
+                    if (!tempMessage.Equals("rcomplete"))
+                    {
+                        Console.WriteLine("Message received: " + encoder.GetString(completeMessage, 0, k));
+                        return;
+                    }
+                    
                     clientImage = b.Resize(320, 240, RewritableBitmap.Interpolation.Bilinear);
                     double tmpXStart = (xStart / 2);
                     double tmpYStart = (yStart / 2);
@@ -108,11 +330,11 @@ namespace Share
 
 
                     int imgSize = smallWidth * smallHeight* 4;
-                    Console.WriteLine("Image size: " + imgSize);
-                    byte[] playerImage = new byte[imgSize];
+                    //Console.WriteLine("Image size: " + imgSize);
+                    byte[] transmitPlayerImage = new byte[imgSize];
 
 
-                    clientImage.CopyPixels(new Int32Rect(xStart, yStart, smallWidth, smallHeight), playerImage, (smallWidth * 4), 0);
+                    clientImage.CopyPixels(new Int32Rect(xStart, yStart, smallWidth, smallHeight), transmitPlayerImage, (smallWidth * 4), 0);
                     //b.CopyPixels(new Int32Rect(xStart, yStart, (xEnd - xStart), (yEnd - yStart)), playerImage, ((xEnd - xStart) * 4), 0);
 
 
@@ -121,13 +343,8 @@ namespace Share
                     byte[] xE = BitConverter.GetBytes(xEnd);
                     byte[] yS = BitConverter.GetBytes(yStart);
                     byte[] yE = BitConverter.GetBytes(yEnd);
-                    byte[] playerImageSize = BitConverter.GetBytes(playerImage.Length);
+                    byte[] playerImageSize = BitConverter.GetBytes(transmitPlayerImage.Length);
 
-                    //Image is too big don't try to send the data
-                    if (encoder.GetString(completeMessage, 0 , k) != "rcomplete")
-                    {
-                        return;
-                    }
 
                     try
                     {
@@ -148,34 +365,23 @@ namespace Share
 
                     s.Send(yE);
 
-                    //WORKING UNCOMMENT AFTER TESTING COMPRESSION
                     s.Send(playerImageSize);
-                    imageSent = true;
                     imageSendCounter = 1;
-                    //byte[] result = new byte[IMAGE_SIZE]; // ARGB
-                    //b.Lock();
-                    //Marshal.Copy(b.BackBuffer, result, 0, IMAGE_SIZE);
-                    //b.Unlock();
-                    //////byte[] compressed = Compressor.Compress(result);
-                    //s.Send(result);
-
-                    s.Send(playerImage);
-                    Console.WriteLine("Image sent, size of image: " + playerImage.Length + "," + xStart + ", " + xEnd + ", " + yStart + ", " + yEnd);
+                    s.Send(transmitPlayerImage);
+                    //Console.WriteLine("Image sent, size of image: " + transmitPlayerImage.Length + "," + xStart + ", " + xEnd + ", " + yStart + ", " + yEnd);
                 }
                 else if (!backgroundSent)
                 {
                     clientImage = b.Resize(320, 240, RewritableBitmap.Interpolation.Bilinear);
-                    byte[] smallBackgroundImage = new byte[IMAGE_SIZE/4];
+                    byte[] smallBackgroundImage = new byte[TRANSMIT_IMAGE_SIZE];
                     clientImage.CopyPixels(new Int32Rect(0, 0, 320, 240), smallBackgroundImage, 320*4, 0);
                     s.Send(smallBackgroundImage);
-                    //s.Send(_sensor.backgroundImage);
                     backgroundSent = true;
                     Console.WriteLine("Background sent");
                 }
                 else
                 {
                     imageSendCounter++;
-                    Console.WriteLine("Image not sent");
                 }
                 
             }
@@ -187,32 +393,28 @@ namespace Share
         {
             if (!clientConnected)
             {
-                //Dispatcher.BeginInvoke((Action)delegate
-                //{
-                    IPAddress ipAddress = IPAddress.Any;
-                    TcpListener listener = new TcpListener(ipAddress, port);
-                    listener.Start();
-                    Console.WriteLine("Server is running");
-                    Console.WriteLine("Listening on port " + port);
-                    Console.WriteLine("Waiting for connections...");
-                    while (!clientConnected)
+                IPAddress ipAddress = IPAddress.Any;
+                TcpListener listener = new TcpListener(ipAddress, portSend);
+                listener.Start();
+                Console.WriteLine("Server is running");
+                Console.WriteLine("Listening on port " + portSend);
+                Console.WriteLine("Waiting for connections...");
+                while (!clientConnected)
+                {
+                    s = listener.AcceptSocket();
+                    s.SendBufferSize = 256000;
+                    Console.WriteLine("Connection accepted from " + s.RemoteEndPoint);
+                    byte[] b = new byte[65535];
+                    int k = s.Receive(b);
+                    ASCIIEncoding enc = new ASCIIEncoding();
+                    Console.WriteLine("Received:" + enc.GetString(b, 0, k) + "..");
+                    //Ensure the client is who we want
+                    if (enc.GetString(b, 0, k) == "hello" || enc.GetString(b, 0, k) == "hellorcomplete")
                     {
-                        s = listener.AcceptSocket();
-                        s.SendBufferSize = 256000;
-                        //s.NoDelay = true;
-                        Console.WriteLine("Connection accepted from " + s.RemoteEndPoint);
-                        byte[] b = new byte[65535];
-                        int k = s.Receive(b);
-                        Console.WriteLine("Received:");
-                        ASCIIEncoding enc = new ASCIIEncoding();
-                        //Ensure the client is who we want
-                        if (enc.GetString(b, 0, k) == "hello")
-                        {
-                            clientConnected = true;
-                            Console.WriteLine(enc.GetString(b, 0, k));
-                        }
+                        clientConnected = true;
+                        Console.WriteLine(enc.GetString(b, 0, k));
                     }
-                //});
+                }
             }
         }
 
@@ -253,6 +455,10 @@ namespace Share
             if (!_serverThread.IsBusy)
             {
                 _serverThread.RunWorkerAsync();
+            }
+            if (!_clientThread.IsBusy)
+            {
+                _clientThread.RunWorkerAsync();
             }
         }
 
